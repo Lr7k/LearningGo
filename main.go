f6809f730cf7c25fcf490120da48d60018c31c70
Lr7k <witgen.thunder@gmail.com>
2024-12-24 16:50:37 +0900

    if文の基礎

--- main.go
+++ main.go
@@ -1,24 +1,60 @@
 package main
 
-import (
-  "fmt"
-)
-
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+import "fmt"
 
 func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	fmt.Println("if文")
+	var num int
+	num = 4
+	if num%2 == 0 {
+		fmt.Println("偶数")
+	} else {
+		fmt.Println("奇数")
+	}
+	fmt.Println("if文 else if")
 
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+	num = 3
+	if num%2 == 0 {
+		fmt.Println("偶数")
+	} else if num%3 == 0 {
+		fmt.Println("3で割り切れます")
+	} else {
+		fmt.Println("その他")
+	}
+
+	fmt.Println("複数条件")
+	var x int
+	var y int
+	x, y = 10, 10
+	if x == 10 && y == 10 {
+		fmt.Println("&&です")
+	}
+	y = 5
+	if x == 10 || y == 10 {
+		fmt.Println("||です")
+	}
+	fmt.Println("if文の条件式で変数宣言")
+	num = 2
+	var res string = by2(num)
+	if res == "OK" {
+		fmt.Println("2で割れる")
+	} else {
+		fmt.Println("2で割れない")
+	}
+	fmt.Println("みにくいからつかわねー")
+	if res2 := by2(num); res2 == "OK" {
+		fmt.Println("2で割れる")
+	} else {
+		fmt.Println("2で割れない")
+	}
+}
+
+func by2(num int) string {
+	if num%2 == 0 {
+		return "OK"
+	} else {
+		return "NG"
+	}
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
